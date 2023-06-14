ae11bc3e15dd81ec038bb05bdd4d561e83fa636b
rezawp <rezaaawp@gmail.com>
2023-06-14 22:58:04 +0700

    update : cron job siang dan malam untuk setiap hari

--- app/jobs/uangkas.job.js
+++ app/jobs/uangkas.job.js
@@ -5,7 +5,8 @@ module.exports = (rkwpbot) => {
 
   // Menjadwalkan cron job untuk menjalankan pada setiap hari Senin dan Jumat pada jam 8 pagi (pukul 8:00)
   //   const cronExpression = "0 8 * * 1,5"; // pada setiap hari senin dan jumat
-  const cronExpression = "50 22 * * *";
+  const cronExpression = "0 8 * * *";
+  const cronExpression2 = "0 21 * * *";
   const timeZone = "Asia/Jakarta";
 
   const job = new CronJob(
@@ -25,5 +26,23 @@ module.exports = (rkwpbot) => {
     timeZone
   );
 
+  const job2 = new CronJob(
+    cronExpression2,
+    async () => {
+      const res = await axios({
+        method: "get",
+        url: "https://mfarels.my.id/api/openai?text=saya%20ingin%20tidur,%20berikan%20saya%20ucapan%20untuk%20selamat%20tidur",
+      });
+      rkwpbot.ev.emit("uangkas", {
+        message: res.data.result,
+        to: "628892732501",
+      });
+    },
+    null,
+    true,
+    timeZone
+  );
+
   job.start();
+  job2.start();
 };
