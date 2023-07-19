2a6d844f788bcdc96b3106c53df61624d8e0af5b
rezawp <rezaaawp@gmail.com>
2023-07-19 10:16:45 +0700

    update

---
+++ app/fiturs/0008_cf.js
@@ -0,0 +1,37 @@
+const logging = require("../../lib/logging");
+const handleMessage = require("../../lib/handleMessage");
+const convertFont = require("../../lib/cf");
+module.exports = async ({
+  rkwpbot,
+  m,
+  bot,
+  type,
+  body,
+  budy,
+  prefix,
+  isCommand,
+  command,
+  isGroup,
+  rkwp,
+  pushname,
+  q,
+  args,
+  content,
+  sender,
+  from,
+  isMedia,
+  isQuotedImage,
+  isQuotedVideo,
+  isQuotedSticker,
+  isQuotedAudio,
+  isLocationMessage,
+}) => {
+  const ev = handleMessage({ rkwpbot, from, bot });
+  try {
+    //** Start Code */
+    await ev.sendMessageText(convertFont(q));
+    //** End Code */
+  } catch (e) {
+    logging("error", "ERROR FITUR cf : " + __dirname + __filename, e);
+  }
+};
