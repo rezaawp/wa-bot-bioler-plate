5b900837c3275257563d853687bbba1daa4de70d
rezawp <rezaaawp@gmail.com>
2023-07-19 10:21:51 +0700

    update

---
+++ font.js
@@ -0,0 +1,27 @@
+const text = "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ";
+const textNormal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const fontBaru = text.split("");
+const fontNormal = textNormal.split("");
+
+function convertFont(text) {
+  text = text.toUpperCase();
+  let fonts = {};
+  for (let i = 0; i < fontNormal.length; i++) {
+    fonts[fontNormal[i]] = fontBaru[i];
+  }
+
+  let result = "";
+  text = text.split("");
+  for (const t of text) {
+    if (t === " ") {
+      result += " ";
+    } else {
+      result += fonts[t];
+    }
+  }
+
+  return result;
+}
+
+console.log(convertFont("cek ch bokep"));
